e5aa4a37abdd189f185b19ba9af00d28f6ca46a7
VincentVVU <kianwu88@gmail.com>
2021-07-07 14:51:58 +1000

    2021/7/7

---
+++ app.js
@@ -0,0 +1,11 @@
+const express =require('express');
+
+const app = express ();
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Node App running...<h1>');
+});
+
+app.listen(5000,()=>{
+    console.log('App listening on port 5000');
+});
